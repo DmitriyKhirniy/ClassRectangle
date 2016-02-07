d23fa39d6cc6c3cf7d8bb81af649c627c41f70c2
Dima Khirniy <DimaKhirniy@gmail.com>
2016-02-07 20:41:00 +0200

    Test commit

---
+++ MainApi.java
@@ -0,0 +1,10 @@
+/**
+ * Created by dima- on 07.02.2016.
+ */
+public class MainApi {
+    public static void main( String[] args )
+    {
+       System.out.print("Hello");
+        System.out.println("World");
+    };
+}
